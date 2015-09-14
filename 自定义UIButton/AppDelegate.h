d569c895691e303a162891a20ce8f90e0e61bc64
Imanol <jk199054@163.com>
2015-09-14 14:44:28 +0800

    Initial Commit

---
+++ 自定义UIButton/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  自定义UIButton
+//
+//  Created by Imanol on 15/9/14.
+//  Copyright (c) 2015年 Imanol. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 自定义UIButtonTests/___UIButtonTests.m
@@ -0,0 +1,40 @@
+//
+//  ___UIButtonTests.m
+//  自定义UIButtonTests
+//
+//  Created by Imanol on 15/9/14.
+//  Copyright (c) 2015年 Imanol. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface ___UIButtonTests : XCTestCase
+
+@end
+
+@implementation ___UIButtonTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
